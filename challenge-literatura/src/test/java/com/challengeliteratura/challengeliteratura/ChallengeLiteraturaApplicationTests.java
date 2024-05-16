c4171021b0a5cd7b6896fb812c79d9e5e4ba4e73
Antonio-Richaud <antonio-richaud@outlook.com>
2024-05-16 13:31:19 -0600

    Se añaden los primeros archivos para empezar con el desarrollo del proyecto

---
+++ challenge-literatura/src/test/java/com/challengeliteratura/challengeliteratura/ChallengeLiteraturaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.challengeliteratura.challengeliteratura;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallengeLiteraturaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
